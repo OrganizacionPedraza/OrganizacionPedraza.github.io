ee8b3daa53a638eeb1d3ca0bc3ea99c4d97a0d14
Haska280 <99769696+Haska280@users.noreply.github.com>
2022-03-22 17:04:33 -0600

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,7 +1,8 @@
 <form>
 <html>
 <body>
-
+<form action"/action_page.php">
+  <form action"/action_page.php" method "post">
 Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
 <?php echo $_POST["lname"]; ?>
